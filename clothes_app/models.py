3c11f8756e74021ac31cf6efff0312bfd0e7af8e
noam106 <117109080+noam106@users.noreply.github.com>
2023-09-12 10:45:52 +0300

    remove pip address

--- clothes_app/models.py
+++ clothes_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
-# from address.address import *
 from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
