5f082558d8657f67819862e7dae0266a14716919
noam106 <117109080+noam106@users.noreply.github.com>
2023-09-14 19:43:57 +0300

    adding MV IP

--- clothes/settings.py
+++ clothes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w*kn(%0n+ris2rqq!5xq2a^a6y5=z4zd%_chjw9kxaor%#=9u8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["34.42.119.5"]
 
 
 # Application definition
