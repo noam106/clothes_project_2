621387c88cf6e6721a33d694ef839158e8764e49
noam106 <117109080+noam106@users.noreply.github.com>
2023-09-27 22:18:27 +0300

    270923

--- clothes_app/urls/items.py
+++ clothes_app/urls/items.py
@@ -1,14 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from clothes_app.views.items import ItemViewSet,create_item
+from clothes_app.views.items import ItemViewSet
 
 router = routers.DefaultRouter()
 router.register(r'', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path("item")
-    #path('img/', upload_item_img),
+
 
 ]
